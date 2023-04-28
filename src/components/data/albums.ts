b2a29b9c86313c6a460ae945741139fe8cbf060b
quesnloops <sirrsewmo@gmail.com>
2023-04-27 22:39:10 -0400

    added more pages

---
+++ src/components/data/albums.ts
@@ -0,0 +1,71 @@
+export interface Album {
+  name: string
+  artist: string
+  cover: string
+}
+
+export const listenNowAlbums: Album[] = [
+  {
+    name: "React Rendezvous",
+    artist: "Ethan Byte",
+    cover:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.architecturaldigest.com%2Fphotos%2F5890e88033bd1de9129eab0a%2F1%3A1%2Fw_870%2Ch_870%2Cc_limit%2FArtist-Designed%2520Album%2520Covers%25202.jpg&tbnid=eRSDN8vhyrL6cM&vet=12ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg..i&imgrefurl=https%3A%2F%2Fwww.architecturaldigest.com%2Fgallery%2Fproof-that-artist-designed-album-covers-are-better-than-the-rest&docid=OrrSvxfoM7vNVM&w=870&h=870&q=album%20covers&ved=2ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg",
+  },
+  {
+    name: "Async Awakenings",
+    artist: "Nina Netcode",
+    cover:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.architecturaldigest.com%2Fphotos%2F5890e88033bd1de9129eab0a%2F1%3A1%2Fw_870%2Ch_870%2Cc_limit%2FArtist-Designed%2520Album%2520Covers%25202.jpg&tbnid=eRSDN8vhyrL6cM&vet=12ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg..i&imgrefurl=https%3A%2F%2Fwww.architecturaldigest.com%2Fgallery%2Fproof-that-artist-designed-album-covers-are-better-than-the-rest&docid=OrrSvxfoM7vNVM&w=870&h=870&q=album%20covers&ved=2ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg",
+  },
+  {
+    name: "The Art of Reusability",
+    artist: "Lena Logic",
+    cover:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.architecturaldigest.com%2Fphotos%2F5890e88033bd1de9129eab0a%2F1%3A1%2Fw_870%2Ch_870%2Cc_limit%2FArtist-Designed%2520Album%2520Covers%25202.jpg&tbnid=eRSDN8vhyrL6cM&vet=12ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg..i&imgrefurl=https%3A%2F%2Fwww.architecturaldigest.com%2Fgallery%2Fproof-that-artist-designed-album-covers-are-better-than-the-rest&docid=OrrSvxfoM7vNVM&w=870&h=870&q=album%20covers&ved=2ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg",
+  },
+  {
+    name: "Stateful Symphony",
+    artist: "Beth Binary",
+    cover:
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.architecturaldigest.com%2Fphotos%2F5890e88033bd1de9129eab0a%2F1%3A1%2Fw_870%2Ch_870%2Cc_limit%2FArtist-Designed%2520Album%2520Covers%25202.jpg&tbnid=eRSDN8vhyrL6cM&vet=12ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg..i&imgrefurl=https%3A%2F%2Fwww.architecturaldigest.com%2Fgallery%2Fproof-that-artist-designed-album-covers-are-better-than-the-rest&docid=OrrSvxfoM7vNVM&w=870&h=870&q=album%20covers&ved=2ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg",
+  },
+]
+
+export const madeForYouAlbums: Album[] = [
+  {
+    name: "Thinking Components",
+    artist: "Lena Logic",
+    cover:
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.architecturaldigest.com%2Fphotos%2F5890e88033bd1de9129eab0a%2F1%3A1%2Fw_870%2Ch_870%2Cc_limit%2FArtist-Designed%2520Album%2520Covers%25202.jpg&tbnid=eRSDN8vhyrL6cM&vet=12ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg..i&imgrefurl=https%3A%2F%2Fwww.architecturaldigest.com%2Fgallery%2Fproof-that-artist-designed-album-covers-are-better-than-the-rest&docid=OrrSvxfoM7vNVM&w=870&h=870&q=album%20covers&ved=2ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg",
+  },
+  {
+    name: "Functional Fury",
+    artist: "Beth Binary",
+    cover:
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.architecturaldigest.com%2Fphotos%2F5890e88033bd1de9129eab0a%2F1%3A1%2Fw_870%2Ch_870%2Cc_limit%2FArtist-Designed%2520Album%2520Covers%25202.jpg&tbnid=eRSDN8vhyrL6cM&vet=12ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg..i&imgrefurl=https%3A%2F%2Fwww.architecturaldigest.com%2Fgallery%2Fproof-that-artist-designed-album-covers-are-better-than-the-rest&docid=OrrSvxfoM7vNVM&w=870&h=870&q=album%20covers&ved=2ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg",
+  },
+  {
+    name: "React Rendezvous",
+    artist: "Ethan Byte",
+    cover:
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.architecturaldigest.com%2Fphotos%2F5890e88033bd1de9129eab0a%2F1%3A1%2Fw_870%2Ch_870%2Cc_limit%2FArtist-Designed%2520Album%2520Covers%25202.jpg&tbnid=eRSDN8vhyrL6cM&vet=12ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg..i&imgrefurl=https%3A%2F%2Fwww.architecturaldigest.com%2Fgallery%2Fproof-that-artist-designed-album-covers-are-better-than-the-rest&docid=OrrSvxfoM7vNVM&w=870&h=870&q=album%20covers&ved=2ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg",
+  },
+  {
+    name: "Stateful Symphony",
+    artist: "Beth Binary",
+    cover:
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.architecturaldigest.com%2Fphotos%2F5890e88033bd1de9129eab0a%2F1%3A1%2Fw_870%2Ch_870%2Cc_limit%2FArtist-Designed%2520Album%2520Covers%25202.jpg&tbnid=eRSDN8vhyrL6cM&vet=12ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg..i&imgrefurl=https%3A%2F%2Fwww.architecturaldigest.com%2Fgallery%2Fproof-that-artist-designed-album-covers-are-better-than-the-rest&docid=OrrSvxfoM7vNVM&w=870&h=870&q=album%20covers&ved=2ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg",
+  },
+  {
+    name: "Async Awakenings",
+    artist: "Nina Netcode",
+    cover:
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.architecturaldigest.com%2Fphotos%2F5890e88033bd1de9129eab0a%2F1%3A1%2Fw_870%2Ch_870%2Cc_limit%2FArtist-Designed%2520Album%2520Covers%25202.jpg&tbnid=eRSDN8vhyrL6cM&vet=12ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg..i&imgrefurl=https%3A%2F%2Fwww.architecturaldigest.com%2Fgallery%2Fproof-that-artist-designed-album-covers-are-better-than-the-rest&docid=OrrSvxfoM7vNVM&w=870&h=870&q=album%20covers&ved=2ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg",
+  },
+  {
+    name: "The Art of Reusability",
+    artist: "Lena Logic",
+    cover:
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.architecturaldigest.com%2Fphotos%2F5890e88033bd1de9129eab0a%2F1%3A1%2Fw_870%2Ch_870%2Cc_limit%2FArtist-Designed%2520Album%2520Covers%25202.jpg&tbnid=eRSDN8vhyrL6cM&vet=12ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg..i&imgrefurl=https%3A%2F%2Fwww.architecturaldigest.com%2Fgallery%2Fproof-that-artist-designed-album-covers-are-better-than-the-rest&docid=OrrSvxfoM7vNVM&w=870&h=870&q=album%20covers&ved=2ahUKEwiel4Pow8v-AhXTookEHTBnA-QQMygAegUIARDbAg",
+  },
+]
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+
+
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "~/styles/globals.css";
